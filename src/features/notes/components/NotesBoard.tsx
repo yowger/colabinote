36225f175b64bb8dbaea698187ea69f62e7d9a42
Ver0018 <rogerpantilyowger@gmail.com>
2026-02-25 02:04:22 +0800

    mini map refactor

--- src/features/notes/components/NotesBoard.tsx
+++ src/features/notes/components/NotesBoard.tsx
@@ -1,4 +1,6 @@
+import { MiniMap } from "../../minimap/Minimap"
 import { useBoardInteraction } from "../hooks/useBoardInteraction"
+import { useNotesStore } from "../hooks/useNotesStore"
 import BoardToolbar from "./NotesBoardToolbar"
 import NotesLayer from "./NotesLayer"
 
@@ -9,11 +11,21 @@ const BOARD_DIMENSIONS = {
 
 export default function NotesBoard() {
     const { viewportRef, handlers, isPanning } = useBoardInteraction()
+    const notes = useNotesStore((s) => s.notes)
 
     return (
         <div className="flex flex-col h-screen">
             <BoardToolbar />
-
+            <div className="absolute z-20 top-0 right-0 m-4">
+                <MiniMap
+                    boardRef={viewportRef}
+                    boardWidth={BOARD_DIMENSIONS.width}
+                    boardHeight={BOARD_DIMENSIONS.height}
+                    width={200} // minimap width
+                    height={100}
+                    notes={Object.values(notes)}
+                />
+            </div>
             <div
                 ref={viewportRef}
                 {...handlers}
